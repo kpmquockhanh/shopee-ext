e3eba1119e5bdaef702b7cb52d633549cb197876
khanhluu <khanhluu@beeketing.net>
2023-10-16 14:34:22 +0700

    Update shopee ext

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from '../view/popup.vue'
 import Antv from 'antv'
-import 'antv/dist/antv.css'
+// import 'antv/dist/antv.css'
 import 'antd-css-utilities/utility.min.css'
 import '../assets/css/app.scss'
 
